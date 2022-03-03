ae6ff1b080e94c9e728512965611b5442fea0e37
Vivanco Qs <86332346+Vivanco443@users.noreply.github.com>
2022-03-03 17:31:55 -0600

    Add código esp32cam

---
+++ ESP32_capstone/src/main.cpp
@@ -0,0 +1,267 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-post-image-photo-server/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+
+const char *ssid = "Heuristicas";   // Totalplay-73A0-_2.4Gnormal   ||    Cuca's_chan
+const char *password = "optimizacion";   // Naylita2021
+
+String serverName = "realdtc.ga"; // SERVER NAME   148.206.74.17
+// String serverName = "example.com";   // OR REPLACE WITH YOUR DOMAIN NAME
+
+String serverPath = "/upload.php"; // The default serverPath /upload.php
+
+const int serverPort = 80;
+
+// button pin
+const int buttonPin = 16;
+int buttonState = HIGH;
+int lastButtonState = LOW;          // the previous reading from the input pin
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
+
+WiFiClient client;
+
+// CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+const int timerInterval = 30000;  // time between each HTTP POST image
+unsigned long previousMillis = 0; // last time image was sent
+camera_config_t config;
+
+int picknum = 5;  // Numero de fotos que se envían
+
+//####################################
+//##### Declaracion de funciones #####
+//####################################
+String sendPhoto();
+void configInitCamera();
+
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP());
+  // Se prepara la cámara
+  configInitCamera();
+  // Se manda la foto
+  //sendPhoto();
+}
+
+void loop()
+{
+  int reading = digitalRead(buttonPin); // Leemos la entrada
+  if (reading != lastButtonState)
+  {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (reading != buttonState) // si el estado del boton ha cambiado:
+    {
+      buttonState = reading;
+      if (buttonState == LOW)
+      {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= timerInterval)    // Tiempo minimo para volver a enviar
+        {
+          Serial.println("¡¡Crushed!!");
+          for(int i=0; i < picknum; i++){
+            sendPhoto();
+            delay(50);
+          }
+          previousMillis = currentMillis;
+        }
+      }
+    }
+  }
+  lastButtonState = reading;
+}
+
+String sendPhoto()
+{
+  String getAll;
+  String getBody;
+  // Las siguientes dos líneas toman la foto
+  camera_fb_t *fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }
+
+  Serial.println("Connecting to server: " + serverName);
+
+  if (client.connect(serverName.c_str(), serverPort))
+  {
+    Serial.println("Connection successful!");     // Esto que onda?? vvv
+    String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--RandomNerdTutorials--\r\n";
+
+    uint32_t imageLen = fb->len;
+    uint32_t extraLen = head.length() + tail.length();
+    uint32_t totalLen = imageLen + extraLen;
+
+    client.println("POST " + serverPath + " HTTP/1.1");
+    client.println("Host: " + serverName);
+    client.println("Content-Length: " + String(totalLen));
+    client.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
+    client.println();
+    client.print(head);
+
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n = 0; n < fbLen; n = n + 1024)
+    {
+      if (n + 1024 < fbLen)
+      {
+        client.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen % 1024 > 0)
+      {
+        size_t remainder = fbLen % 1024;
+        client.write(fbBuf, remainder);
+      }
+    }
+    client.print(tail);
+
+    esp_camera_fb_return(fb);
+
+    int timoutTimer = 10000;
+    long startTimer = millis();
+    boolean state = false;
+
+    while ((startTimer + timoutTimer) > millis())
+    {
+      Serial.print(".");
+      delay(100);
+      while (client.available())
+      {
+        char c = client.read();
+        if (c == '\n')
+        {
+          if (getAll.length() == 0)
+          {
+            state = true;
+          }
+          getAll = "";
+        }
+        else if (c != '\r')
+        {
+          getAll += String(c);
+        }
+        if (state == true)
+        {
+          getBody += String(c);
+        }
+        startTimer = millis();
+      }
+      if (getBody.length() > 0)
+      {
+        break;
+      }
+    }
+    Serial.println();
+    client.stop();
+    Serial.println(getBody);
+  }
+  else    // Si no logra hacer la conexión FTP
+  {
+    getBody = "Connection to " + serverName + " failed.";
+    Serial.println(getBody);
+  }
+  return getBody;
+}
+
+void configInitCamera()
+{
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 10; // 0-63 lower number means higher quality
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.frame_size = FRAMESIZE_CIF;
+    config.jpeg_quality = 12; // 0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);   // Checa esta línea
+    ESP.restart(); // Checa esta linea (no está en el otro código)
+  }
+}
