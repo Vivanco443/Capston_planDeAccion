7e3891ceea6525a13082a0e1b9c6e50fde556ed8
Vivanco Qs <86332346+Vivanco443@users.noreply.github.com>
2022-04-19 16:38:11 -0500

    Un unico sensor, una unica salida
    Se modifica el script para que solo se lea un puerto como sensor y solo haya una salida hacia el esp32-cam

--- Arduin_capstone/Arduin_capstone.ino
+++ Arduin_capstone/Arduin_capstone.ino
@@ -1,46 +1,39 @@
+/*
+A0  -   Señal sensor 
+2   -   Salida a ESP 
+
+*/
+
 #include <Arduino.h>
-#define fron_sens 0   //  A0 pin
-#define back_sens 1   //  A1 pin
-#define fronpin 2    //  Digital output
-#define backpin 3    //  Digital output
+#define sensor 0   //  A0 pin
+#define signalpin 2    //  Digital output
 #define ledpin 13
-int fronSenVal = 0;
-int backSenVal = 0;
+int senVal = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(fronpin, OUTPUT);
-  pinMode(backpin, OUTPUT);
+  //  Inicializa puerto serie
+  Serial.begin(9600);   
+  //  Declara puertos de salida
+  pinMode(signalpin, OUTPUT);
   pinMode(ledpin, OUTPUT);
-  digitalWrite(fronpin, LOW);
-  digitalWrite(backpin, LOW);
+  digitalWrite(signalpin, LOW);
   digitalWrite(ledpin, LOW);
 }
 
 void loop()
 {
-  // Hace lectura analógica de cada sensor
-  fronSenVal = analogRead(fron_sens);   
-  backSenVal = analogRead(back_sens);
+  // Hace lectura analógica del sensor
+  senVal = analogRead(sensor);
 
   // Pregunta si han cruzado el umbral
-  if (fronSenVal < 1000)
+  if (senVal < 1000)
   {
-    digitalWrite(fronpin, HIGH); 
+    digitalWrite(signalpin, HIGH); 
     digitalWrite(ledpin, HIGH); 
+    Serial.println("Activacion del sensor");
     delay(500);    // Envía una señal el tiempo suficiente para  que la detecte
-    digitalWrite(fronpin, LOW);
-    digitalWrite(ledpin, LOW);
-    delay(500);
-  }
-  // Hace lo mismo pero para el sensor trasero
-  else if (backSenVal < 1000)
-  {
-    digitalWrite(backpin, HIGH);
-    digitalWrite(ledpin, HIGH);
-    delay(500);
-    digitalWrite(backpin, LOW);
+    digitalWrite(signalpin, LOW);
     digitalWrite(ledpin, LOW);
     delay(500);
   }
